4d470047a0f23ff6c9953abdb0c404840224c77b
Jai Narayanan <47904405+JaiNarayanan@users.noreply.github.com>
2024-02-15 13:15:37 -0500

    chore: project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,42 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
+import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
+import Editor from '@monaco-editor/react';
+import { useRef } from "react";
+import DnDFlow from "./components/canvas/ReactFlowDnd";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const editorRef = useRef(null);
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
+  function handleEditorDidMount(editor, monaco) {
+    editorRef.current = editor;
   }
 
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
+    <div style={{ display: 'flex' }}>
+      <PanelGroup direction="horizontal">
+        <Panel defaultSize={50} minSize={30}>
+          <Editor
+            height="90vh"
+            defaultLanguage="json"
+            defaultValue="// some comment"
+            onMount={handleEditorDidMount}
+          />
+        </Panel>
+        <PanelResizeHandle style={{
+          flex: '0 0 0.25rem',
+          // display: 'flex',
+          // justifyContent: 'stretch',
+          alignItems: 'stretch',
+          outline: 'none',
+          transition: 'background-color 0.2s linear',
+          backgroundColor: 'green'
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
+        }} />
+        <PanelResizeHandle />
+        <Panel defaultSize={50} minSize={20}>
+          <DnDFlow />
+        </Panel>
+      </PanelGroup>
     </div>
   );
 }
